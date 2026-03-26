e70b4f5fd9656b4f7009b6b91e968199c1682fa0
KayongoRyan <kayongoryan2020@gmail.com>
2026-03-26 12:06:10 +0200

    Revert "Add Flutter app port (TAM): Riverpod, go_router, Firebase RTDB, maps"
    This reverts commit 3c7da081944586b9c2fea06d3559779d845db186.

--- flutter_app/lib/providers/destinations_provider.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-
-import '../domain/rwanda_destinations_loader.dart';
-import '../domain/rwanda_pricing.dart';
-
-final rwandaDestinationsProvider = FutureProvider<List<RwandaDestination>>((ref) async {
-  return loadRwandaDestinations();
-});
